1bc6e10ce96874882a49fae6065937adde4be47c
cballew96 <38291954+cballew96@users.noreply.github.com>
2023-01-17 12:41:10 -0600

    Create longest_substring.py
    returns longest substring with no repeating characters

---
+++ longest_substring.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s: str):
+        longest = ''
+        flongest = ''
+        size = 0
+        for val in s:
+            if val in longest:
+                size = len(longest)
+                if len(longest) > len(flongest):
+                    flongest = longest
+
+                longest = '' + val
+            else:
+                longest += val
+        if len(longest) > len(flongest):
+            flongest = longest
+            size = len(flongest)
+
+        return (flongest, size)
+
+
+c1 = Solution()
+c2 = Solution()
+c3 = Solution()
+
+print(c1.lengthOfLongestSubstring('abcabcdabcdef')) # returns ('abcdef', 6)
+print(c2.lengthOfLongestSubstring('bbbbb')) # returns ('b', 1)
+print(c3.lengthOfLongestSubstring('pwwkew')) # returns ('wke', 3)
